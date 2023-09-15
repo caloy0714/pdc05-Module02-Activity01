bdc0debfa865af76118794527ed4e9bb9c2f03d7
unknown <caloy0714@gmail.com>
2023-09-15 19:01:16 +0800

    first commit

---
+++ binding/binding/Model/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace binding.Model
+{
+    class TaskModel
+    {
+        public string Subjectcode { get; set; }
+
+        public string SubjectTitle { get; set; }
+
+        public int Unit { get; set; }
+
+    }
+}
